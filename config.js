addaefa1635808f2ec1638907cc23dadad4bee2d
Dara Hayes <dara.hayes@nearform.com>
2016-09-23 16:00:46 +0100

    fixed default credential naming && bumped revision

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ module.exports = {
   scope: ['https://spreadsheets.google.com/feeds'],
   nameRange: 'A1:CI1',
   dateRange: 'D1:D1000',
-  clientEmail: process.env.SHEETS_CLIENT_EMAIL,
-  secretKey: process.env.SHEETS_PRIVATE_KEY
+  client_email: process.env.SHEETS_CLIENT_EMAIL,
+  private_key: process.env.SHEETS_PRIVATE_KEY
 }
